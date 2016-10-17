9337192d72795fa55ed7d83f7b47b76c8bff76c2
Victor Delannoy <victor.delannoy@gmail.com>
2016-10-17 12:09:20 +0200

    sinatra v0

--- app.rb
+++ app.rb
@@ -2,11 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry-byebug'
 require 'better_errors'
+set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+# https://github.com/lewagon/sinatra-101#share-with-the-world
+
 get '/' do
-  'Hello world!'
+  erb :index
+end
+
+get '/about' do
+  @usernames = ['victor', 'maxime', 'julien']
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
